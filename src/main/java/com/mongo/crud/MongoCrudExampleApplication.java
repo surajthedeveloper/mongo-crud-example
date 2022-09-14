1108a77427615f7ecf490781f473dc8a01b65bbd
Suraj Janu <suraj.jannu@publicissapient.com>
2022-09-14 13:14:40 +0530

    project setup

---
+++ src/main/java/com/mongo/crud/MongoCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoCrudExampleApplication.class, args);
+	}
+
+}
